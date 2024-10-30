1cb60d7d023f5c06b6516f69750aeb44b85f168a
mateuszmatejkokurowski <mateusz.kurowski@hftm.ch>
2024-10-30 22:31:16 +0100

    feat: updated app.component and blog-card.component, created post-db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BlogCardComponent } from './blog-card/blog-card.component';
+import { BlogPost } from './models/blog-post.model';
+import { BlogService } from './services/blog.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FormsModule, BlogCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, BlogCardComponent],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  posts: BlogPost[] | undefined;
+
+  constructor(private blogService: BlogService) {}
+
+  ngOnInit(): void {
+    this.blogService.getPosts().subscribe(
+      (response) => {
+        this.posts = response.data;
+        console.log(this.posts);
+      },
+      (error) => {
+        console.error('Error fetching posts:', error);
+      },
+    );
+  }
+}
