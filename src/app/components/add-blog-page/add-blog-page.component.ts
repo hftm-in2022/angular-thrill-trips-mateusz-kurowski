6393b9bc85d3e6df63d8c484810c670124939590
mateuszmatejkokurowski <mateusz.kurowski@hftm.ch>
2025-01-04 14:38:24 +0100

    feat: sprint 5, created form for adding blog entry

--- src/app/components/add-blog-page/add-blog-page.component.ts
+++ src/app/components/add-blog-page/add-blog-page.component.ts
@@ -1,8 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { BlogService } from '../../core/services/blog.service';
 
 @Component({
   selector: 'app-add-blog-page',
-  template: `<h1>Add Blog Page</h1>`,
-  standalone: false,
+  templateUrl: './add-blog-page.component.html',
+  styleUrls: ['./add-blog-page.component.scss'],
 })
-export class AddBlogPageComponent {}
+export class AddBlogPageComponent {
+  blogForm: FormGroup;
+
+  constructor(
+    private form: FormBuilder,
+    private blogService: BlogService,
+    private router: Router,
+  ) {
+    this.blogForm = this.form.group({
+      title: ['', [Validators.required, Validators.minLength(5)]],
+      content: ['', [Validators.required, Validators.minLength(10)]],
+      headerImageUrl: [''],
+    });
+  }
+
+  get title() {
+    return this.blogForm.get('title');
+  }
+
+  get content() {
+    return this.blogForm.get('content');
+  }
+
+  onSubmit(): void {
+    if (this.blogForm.valid) {
+      const newBlog = this.blogForm.value;
+      this.blogService.savePost(newBlog).subscribe({
+        next: () => this.router.navigate(['']),
+        error: (err) => console.error('Failed to create blog:', err),
+      });
+    }
+  }
+
+  navigateToHome(): void {
+    console.log('Navigating to home...');
+    this.router.navigate(['/']);
+  }
+}
--- src/app/components/add-blog-page/add-blog-page.module.ts
+++ src/app/components/add-blog-page/add-blog-page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AddBlogPageComponent } from './add-blog-page.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AddBlogPageComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{ path: '', component: AddBlogPageComponent }]),
   ],
 })
